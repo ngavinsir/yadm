c48596fd7c71cc035f57ed5ea3d2a6b184b39f88
ngavinsir <ngavinsir@gmail.com>
2024-05-12 13:57:25 +0700

    first

---
+++ .config/nvim/lua/plugins/go.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = false,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports" },
+      },
+    },
+  },
+}
